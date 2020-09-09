1412cc8fb5bd5c0358398987b81cde87f93a8764
falstack <icesilt@outlook.com>
2020-09-09 16:32:55 +0800

    fixed: refresh

--- src/actions.js
+++ src/actions.js
@@ -73,7 +73,7 @@ export const initData = ({
   const params = generateRequestParams({
     field: {
       ...fieldData,
-      fetched: !doRefresh
+      fetched: false
     },
     uniqueKey,
     query,
